1d65f56e35efb0cc14957c5bc2f6f247341c613a
Jamie Chiu <jamiechiu21@gmail.com>
2024-10-30 19:35:23 -0400

    Added server-side form validation

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,24 +1,30 @@
-const express = require('express');
-const { Server } = require('socket.io')
-const helmet = require('helmet')
+const express = require("express");
+const { Server } = require("socket.io");
+const helmet = require("helmet");
+const cors = require("cors");
+const authRouter = require("./routers/authRouter")
 
 const app = express();
-const server = require("http").createServer(app)
+const server = require("http").createServer(app);
 
 // Attach socket.io server to http server to handle WebSocket connections
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://localhost:5173",
         credentials: "true",
     }
 });
 
-app.use(helmet());
+app.use(helmet()); // Sets HTTP headers for security
+
+app.use(cors({
+    origin: "http://localhost:5173", // Only allows requests from localhost:5173
+    credentials: true // Allows requests with credentials (cookies)
+}));  
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json("hi")
-});
+app.use("/auth", authRouter) // For /auth/login and /auth/register
 
 io.on("connect", (socket)=> {});
 
